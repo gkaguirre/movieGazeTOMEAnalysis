e1df327b53e6e40e30f8de98c7872513c115d416
gkaguirre <gkaguirre@me.com>
2020-08-17 20:40:24 -0400

    Loop over acquisitions
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/gkaSnippets/cleanVq.m
+++ code/gkaSnippets/cleanVq.m
@@ -2,76 +2,108 @@
 % Load the gaze data file from my local disk
 load('/Users/aguirre/Documents/MATLAB/projects/movieGazeTOMEAnalysis/data/gazeData.mat')
 
-% Extract the data for the first acquisition
-vq = gazeData.tfMRI_MOVIE_AP_run01.vq;
+% These are the fields to process
+fieldNames = {'tfMRI_MOVIE_AP_run01','tfMRI_MOVIE_AP_run02','tfMRI_MOVIE_PA_run03','tfMRI_MOVIE_PA_run04'};
 
-% How many subjects do we have
-nSubs = size(vq,1);
-
-% How many measures do we have?
-nMeasures = size(vq,2);
-
-% Store a record of the location of the nan values
-vqNaN = isnan(vq);
-
-% This is the mean across all subjects / time points for each measure
-vqMeanVal = squeeze(nanmean(nanmean(vq),3));
-
-% These are the mean centered vectors
-vqCentered = vq - nanmean(vq,3);
-
-% This is the mean vector across all subjects for each measure
-vqMeanVec = squeeze(nanmean(vqCentered));
-
-% Find the slope that relates each individual subject to the mean vector
-for mm = 1:nMeasures
-    for ii = 1:nSubs
-        goodIdx = ~isnan(vqCentered(ii,mm,:));
-        p = polyfit(squeeze(vqCentered(ii,mm,goodIdx)),vqMeanVec(mm,goodIdx)', 1);
-        slopes(mm,ii) = p(1);
+% Loop over the fieldNames
+for ff = 1:length(fieldNames)
+    
+    % Extract the data for the first acquisition
+    vq = gazeData.(fieldNames{ff}).vq;
+    
+    % How many subjects do we have
+    nSubs = size(vq,1);
+    
+    % How many measures do we have?
+    nMeasures = size(vq,2);
+    
+    % Store a record of the location of the nan values
+    vqNaN = isnan(vq);
+    
+    % This is the mean across all subjects / time points for each measure
+    vqMeanVal = squeeze(nanmean(nanmean(vq),3));
+    
+    % These are the mean centered vectors
+    vqCentered = vq - nanmean(vq,3);
+    
+    % Find the phase shift in the first measure that best aligns all
+    % vectors
+%     frameShifts = -60:60;
+%     for xx=1:nSubs
+%         for yy = 1:nSubs
+%             vecA = squeeze(vqCentered(xx,1,:));
+%             vecB = squeeze(vqCentered(yy,1,:));
+%             for tt = 1:length(frameShifts)
+%                 corVals(tt) = corr(vecA,circshift(vecB,frameShifts(tt)),'Rows','pairwise');
+%             end
+%             cr(xx,yy) = max(corVals);
+%             cc(xx,yy) = frameShifts(find(corVals==max(corVals),1));
+%         end
+%     end
+%     cc(find(eye(nSubs,nSubs)))=nan;
+%     cr(find(eye(nSubs,nSubs)))=nan;
+%     figure
+%     subplot(1,2,1)
+%     imagesc(cr)
+%     subplot(1,2,2)
+%     imagesc(cc)
+%     nanmean(nanmean(cr))
+    
+    % This is the mean vector across all subjects for each measure
+    vqMeanVec = squeeze(nanmean(vqCentered));
+    
+    % Find the slope that relates each individual subject to the mean vector
+    for mm = 1:nMeasures
+        for ii = 1:nSubs
+            goodIdx = ~isnan(vqCentered(ii,mm,:));
+            p = polyfit(squeeze(vqCentered(ii,mm,goodIdx)),vqMeanVec(mm,goodIdx)', 1);
+            slopes(mm,ii) = p(1);
+        end
+        % Adjust the mean vector to remove compression
+        vqMeanVec(mm,:) = vqMeanVec(mm,:) ./ mean(slopes(mm,:));
     end
-    % Adjust the mean vector to remove compression
-    vqMeanVec(mm,:) = vqMeanVec(mm,:) ./ mean(slopes(mm,:));
-end
-
-% Now loop through and adjust each subject's data
-
-vqCenteredScaled = nan(size(vqCentered));
-% Find the slope that relates each individual subject to the mean vector
-for mm = 1:nMeasures
-    for ii = 1:nSubs
-        goodIdx = ~isnan(vqCentered(ii,mm,:));
-        p = polyfit(squeeze(vqCentered(ii,mm,goodIdx)),vqMeanVec(mm,goodIdx)', 1);
-        slopes(mm,ii) = p(1);
-        vqCenteredScaled(ii,mm,goodIdx) = vqCentered(ii,mm,goodIdx).*p(1);
+    
+    % Now loop through and adjust each subject's data
+    vqCenteredScaled = nan(size(vqCentered));
+    for mm = 1:nMeasures
+        for ii = 1:nSubs
+            goodIdx = ~isnan(vqCentered(ii,mm,:));
+            % Find the slope that relates each individual subject to the mean vector
+            p = polyfit(squeeze(vqCentered(ii,mm,goodIdx)),vqMeanVec(mm,goodIdx)', 1);
+            slopes(mm,ii) = p(1);
+            vqCenteredScaled(ii,mm,goodIdx) = vqCentered(ii,mm,goodIdx).*p(1);
+        end
+        vqCenteredScaledSD(mm,:) = squeeze(nanstd(vqCenteredScaled(:,mm,:)))';
     end
-    vqCenteredScaledSD(mm,:) = squeeze(nanstd(vqCenteredScaled(:,mm,:)))';
-end
-
-% Create a cleaned vq matrix that has "imputed" missing values with the
-% mean across subject value, and adds back in the mean
-vqCleaned = vqCenteredScaled;
-for mm = 1:nMeasures
-    for ii = 1:nSubs
-        badIdx = isnan(vqCenteredScaled(ii,mm,:));
-        vqCleaned(ii,mm,badIdx) = vqMeanVec(mm,badIdx);
-        
-        % add back in the mean to the whole vector
-        vqCleaned(ii,mm,:) = vqCleaned(ii,mm,:) + vqMeanVal(mm);
+    
+    % Create a cleaned vq matrix that has "imputed" missing values with the
+    % mean across subject value, and adds back in the mean
+    vqCleaned = vqCenteredScaled;
+    for mm = 1:nMeasures
+        for ii = 1:nSubs
+            badIdx = isnan(vqCenteredScaled(ii,mm,:));
+            vqCleaned(ii,mm,badIdx) = vqMeanVec(mm,badIdx);
+            
+            % add back in the mean to the whole vector
+            vqCleaned(ii,mm,:) = vqCleaned(ii,mm,:) + vqMeanVal(mm);
+        end
     end
-end
-
-% Create a display of the data
-figure
-titles={'x gaze','y gaze','stop radius','nans'};
-for mm = 1:size(vq,2)
-    subplot(nMeasures+1,1,mm)
-    imagesc(squeeze(vqCleaned(:,mm,:)));
+    
+    % Store the vqCleaned matrix back in the gazeData structure
+    gazeData.(fieldNames{ff}).vqCleaned = vqCleaned;
+    
+    % Create a display of the data
+    figure
+    titles={'x gaze','y gaze','stop radius','nans'};
+    for mm = 1:size(vq,2)
+        subplot(nMeasures+1,1,mm)
+        imagesc(squeeze(vqCleaned(:,mm,:)));
+        axis off
+        title(titles{mm});
+    end
+    subplot(nMeasures+1,1,mm+1)
+    imagesc(squeeze(vqNaN(:,1,:)));
     axis off
-    title(titles{mm});
+    title(titles{mm+1});
+    
 end
-subplot(nMeasures+1,1,mm+1)
-imagesc(squeeze(vqNaN(:,1,:)));
-axis off
-title(titles{mm+1});
-
--- code/gkaSnippets/eyePositionMovie.m
+++ code/gkaSnippets/eyePositionMovie.m
@@ -1,47 +1,71 @@
 
+
+% These are the fields to process
+fieldNames = {'tfMRI_MOVIE_AP_run01','tfMRI_MOVIE_AP_run02','tfMRI_MOVIE_PA_run03','tfMRI_MOVIE_PA_run04'};
+
+% The movie start times (in seconds) for each of the acquisitions
+movieStartTimes = [1880, 2216, 892, 1228];
+
+% Account for a half-second phase shift that appears to be present between
+% the eye tracking and the movie
+phaseCorrect = -0.25;
+
+% How long a trail (in frames) do we leave behind each tracking circle?
+nTrail = 0;
+
 % Convert gaze coodinates and stop radius to screen coordinates
 screenCoord = @(gazeCoord) (-gazeCoord).*(1080/20.8692) + [1920 1080]/2;
 symbolRadius = @(relRad) (relRad - min(mean(vqCleaned(:,3,:)))) * 30;
 
-% Set up the video in and out
+% Set up the video in
 v = VideoReader('/Users/aguirre/Dropbox (Aguirre-Brainard Lab)/TOME_materials/StimulusFiles/PixarShorts.mov');
-vo = VideoWriter('/Users/aguirre/Desktop/GazeTrack.mov');
 
-% Set up the timebase. Account for a half-second phase shift that appears
-% to be present between the eye tracking and the movie
-movieStartTime = 1880;
-phaseCorrect = -0.25;
-timebaseSecs = (gazeData.timebase./1000) + movieStartTime + phaseCorrect;
-
-nTrail = 0;
-
-% Set up the symbol colors
-nSubs = size(vqCleaned,1);
-colors = getDistinguishableColors(nSubs).*255;
-
-% Set the frame rate for the output
-vo.FrameRate = 1/(timebaseSecs(2)-timebaseSecs(1));
-
-% Open the video out object
-open(vo);
-
-% Loop through the frames
-for tt = 1:length(timebaseSecs)
-    v.CurrentTime=timebaseSecs(tt);
-    f = readFrame(v);
-    for ss = 1:nSubs
-        thisCoord = screenCoord(squeeze(vqCleaned(ss,1:2,tt)));
-        thisRadius = symbolRadius(vqCleaned(ss,3,tt));
-        f = insertShape(f,'circle',[thisCoord thisRadius],'Color',colors(ss,:));
-        for rr = 1:min([nTrail tt-1])
-            lineCoords = [screenCoord(squeeze(vqCleaned(ss,1:2,tt-rr+1))) ...
-                screenCoord(squeeze(vqCleaned(ss,1:2,tt-rr)))];
-            f = insertShape(f,'line',lineCoords,'LineWidth',3,'Color',colors(ss,:));            
+% Loop over the fieldNames
+for ff = 1:length(fieldNames)
+    
+    % Get this cleaned matrix
+    vqCleaned = gazeData.(fieldNames{ff}).vqCleaned;
+        
+    % Set up the timebase.
+    timebaseSecs = (gazeData.timebase./1000) + movieStartTimes(ff) + phaseCorrect;
+    
+    % Set up the symbol colors
+    nSubs = size(vqCleaned,1);
+    colors = getDistinguishableColors(nSubs).*255;
+    
+    % Set up the video out
+    fileNameOut = sprintf('/Users/aguirre/Desktop/GazeTrack_%02d.avi',ff);
+    vo = VideoWriter(fileNameOut);
+    
+    % Set the frame rate for the output
+    vo.FrameRate = 1/(timebaseSecs(2)-timebaseSecs(1));
+    
+    % Open the video out object
+    open(vo);
+    
+    % Loop through the frames
+    for tt = 1:length(timebaseSecs)
+        v.CurrentTime=timebaseSecs(tt);
+        f = readFrame(v);
+        for ss = 1:nSubs
+            thisCoord = screenCoord(squeeze(vqCleaned(ss,1:2,tt)));
+            thisRadius = symbolRadius(vqCleaned(ss,3,tt));
+            if ~any(isnan([thisCoord thisRadius]))
+                f = insertShape(f,'circle',[thisCoord thisRadius],'LineWidth',2,'Color',colors(ss,:));
+                for rr = 1:min([nTrail tt-1])
+                    lineCoords = [screenCoord(squeeze(vqCleaned(ss,1:2,tt-rr+1))) ...
+                        screenCoord(squeeze(vqCleaned(ss,1:2,tt-rr)))];
+                    f = insertShape(f,'line',lineCoords,'LineWidth',3,'Color',colors(ss,:));
+                end
+            end
         end
+        writeVideo(vo,f)
     end
-    writeVideo(vo,f)
+    
+    % Close and clear the video objects
+    close(vo);
+    clear vo
+    
 end
 
-% Close and clear the video objects
-close(vo);
-clear v vo
+clear v
